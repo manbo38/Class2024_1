1a3bd95df15356d030d9ab63ad1ca7fecffa6f84
파크종찬 <ziastar@naver.com>
2024-09-05 19:52:45 +0900

    class

---
+++ src/p2024_09_05/BackJoon_240905_2.java
@@ -0,0 +1,26 @@
+package p2024_09_05;
+
+import java.util.Scanner;
+
+public class BackJoon_240905_2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		String[] s = new String[5];
+		
+		for(int i=0; i<s.length; i++) {
+			s[i] = sc.nextLine();
+		}
+		
+		for(int i=0; i<15; i++) {
+			for(int j=0; j<s.length; j++) {
+				if(i<s[j].length()) {
+					sb.append(s[j].charAt(i));
+				}
+			}
+		}
+		System.out.print(sb.toString());
+	}
+
+}
