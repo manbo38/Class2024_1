e16d6a88e94651a18cea989c2ab4f0dd256896cb
파크종찬 <ziastar@naver.com>
2024-09-09 19:51:00 +0900

    class

---
+++ src/p2024_09_09/BackJoon_240909_3.java
@@ -0,0 +1,27 @@
+package p2024_09_09;
+
+import java.io.*;
+import java.util.*;
+
+public class BackJoon_240909_3 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        int quarter = 25; 
+        int dime = 10;  
+        int nickel = 5;  
+        int penny = 1;  
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < t; i++) {
+            int c = Integer.parseInt(br.readLine());
+            sb.append(c / quarter + " ");
+            c %= quarter;
+            sb.append(c / dime + " ");
+            c %= dime;
+            sb.append(c / nickel + " ");
+            c %= nickel;
+            sb.append(c / penny + "\n");
+        }
+        System.out.print(sb);
+    }
+}
