32313d39e5b0101e63f54285b893f0bcb8b414cf
manbo38 <ziastar@naver.com>
2024-09-17 03:13:22 +0900

    Add files via upload

---
+++ src/p2024_09_12/BackJoon_240912_1.java
@@ -0,0 +1,28 @@
+package p2024_09_12;
+
+import java.util.Scanner;
+
+public class BackJoon_240912_1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int line = 0;
+		int sum = 0;
+		
+		while(sum < n) {
+			line++;
+			sum += line;
+		}
+		
+		int b = sum - n;
+		
+		if(line % 2 == 0) {
+			System.out.println((line - b) + "/" + (b + 1));
+		}else {
+			System.out.println((b + 1) + "/" + (line - b));
+		}
+	}
+
+}
